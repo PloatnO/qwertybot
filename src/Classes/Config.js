5888a7ea674e4f83cfa47f3cf37eee341f070b64
BouncyRocket <jtsc12@outlook.com>
2024-11-26 21:02:40 +0000

    Fixed config

--- src/Classes/Config.js
+++ src/Classes/Config.js
@@ -3,6 +3,7 @@ const yaml = require('yaml');
 const path = require('path');
 const { Logger } = require('./Logger');
 const configFilePath = path.join(__dirname, '../config.yaml');
+const templateConfigFilePath = path.join(__dirname, '../config_template.yaml');
 
 class Config {
     constructor() {
@@ -11,6 +12,10 @@ class Config {
 
     async init() {
         try {
+            if (await fs.readFile(templateConfigFilePath, 'utf8') && !process.argv[2] === '--no-config-warn') {
+                this.logger.warn('U dumbass u forgot to rename the config file')
+                return;
+            }
             const nonParsedConfig = await fs.readFile(configFilePath, 'utf8');
             const parsedConfig = yaml.parse(nonParsedConfig);
             return parsedConfig;
